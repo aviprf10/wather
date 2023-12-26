d376c8f1ae989af3ef69654be7f3c1c547aa9120
Avinash Tiwari <aviprf10@gmail.com>
2023-12-26 06:35:53 +0530

    code

--- src/App.js
+++ src/App.js
@@ -1,57 +1,13 @@
-// WeatherApp.js
-import React, { useState } from 'react';
-import axios from 'axios';
+// src/App.js
+import React from 'react';
+import WeatherApp from './WeatherApp';
 
-const WeatherApp = () => {
-  const [city, setCity] = useState('');
-  const [weatherData, setWeatherData] = useState(null);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
-
-  const API_KEY = 'fd453e05fcc64b5786e04442232411'; // Replace with your actual API key
-
-  const handleSearch = async () => {
-    if (!city) return;
-
-    setLoading(true);
-
-    try {
-      const response = await axios.get(
-        `https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${city}`
-      );
-      setWeatherData(response.data);
-    } catch (error) {
-      setError('Failed to fetch weather data');
-      alert('Failed to fetch weather data');
-    } finally {
-      setLoading(false);
-    }
-  };
-
+function App() {
   return (
-    <div>
-      <input
-        type="text"
-        placeholder="Enter city name"
-        value={city}
-        onChange={(e) => setCity(e.target.value)}
-      />
-      <button onClick={handleSearch}>Search</button>
-
-      {loading && <p>Loading data...</p>}
-
-      {weatherData && (
-        <div className="weather-card">
-          <p>Temperature: {weatherData.current.temp_c}°C</p>
-          <p>Humidity: {weatherData.current.humidity}%</p>
-          <p>Condition: {weatherData.current.condition.text}</p>
-          <p>Wind Speed: {weatherData.current.wind_kph} km/h</p>
-        </div>
-      )}
-
-      {error && <p>{error}</p>}
+    <div className="App">
+      <WeatherApp />
     </div>
   );
-};
+}
 
-export default WeatherApp;
+export default App;
---
+++ src/WeatherApp.js
@@ -0,0 +1,58 @@
+// src/App.js
+import React, { useState } from 'react';
+import axios from 'axios';
+
+const WeatherApp = () => {
+  const [city, setCity] = useState('');
+  const [weatherData, setWeatherData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const API_KEY = 'fd453e05fcc64b5786e04442232411'; // Replace with your actual API key from https://www.weatherapi.com/
+
+  const handleSearch = async () => {
+    if (!city) return;
+  
+    setLoading(true);
+    try {
+        const response = await axios.get(
+          `https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${city}`
+        );
+        setWeatherData(response.data);
+      } catch (error) {
+        console.error('Error fetching weather data:', error);
+        setError('Failed to fetch weather data');
+        alert('Failed to fetch weather data');
+      } finally {
+        setLoading(false);
+      }
+  };
+  
+
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="Enter city name"
+        value={city}
+        onChange={(e) => setCity(e.target.value)}
+        />
+      <button onClick={handleSearch}>Search</button>
+
+      {loading && <p>Loading data...</p>}
+
+      {weatherData && (
+        <div className="weather-card">
+          <p>Temperature: {weatherData.current.temp_c}°C</p>
+          <p>Humidity: {weatherData.current.humidity}%</p>
+          <p>Condition: {weatherData.current.condition.text}</p>
+          <p>Wind Speed: {weatherData.current.wind_kph} km/h</p>
+        </div>
+      )}
+
+      {error && <p>{error}</p>}
+    </div>
+  );
+};
+
+export default WeatherApp;
